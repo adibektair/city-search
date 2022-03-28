569c0119270d7cad44a7057687bdde68e9aca575
Tair Adibek <adibek.tair@gmail.com>
2022-03-28 18:46:00 +0300

    Added new protocol to reusable view and customized cells in Cities list

---
+++ City-Search/Scenes/MainScene/Cells/CityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CityTableViewCell.swift
+//  City-Search
+//
+//  Created by tair on 28.03.22.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
